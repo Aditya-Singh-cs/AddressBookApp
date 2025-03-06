dfa8fe5f51e423d529cc3209258e97b926477c4e
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-06 15:06:53 +0530

    Added Model and DTO

---
+++ src/main/java/com/example/AddressBookApp/dto/AddressBookDTO.java
@@ -0,0 +1,12 @@
+package com.example.AddressBookApp.dto;
+
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBookDTO {
+    private Long id;
+    private String name;
+    private String phone;
+}
