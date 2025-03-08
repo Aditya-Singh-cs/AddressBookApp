8a235dc105a20ef8fe13c5b6d7e2e9edc60f7581
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-08 22:34:52 +0530

    Added application properties for developer and production

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -12,7 +12,7 @@ public class AddressBookAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
 //		log.info("Address Book App Started");
-		log.info("Employee Payroll App started in {} environment", context.getEnvironment().getProperty("environment"));
+		log.info("Address Book App started in {} environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
