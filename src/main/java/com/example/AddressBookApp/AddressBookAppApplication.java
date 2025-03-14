7a91096c17bcb3d5d3a6878214fbbc95682464b6
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-14 22:46:05 +0530

    Added Redis

--- src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
+++ src/main/java/com/example/AddressBookApp/AddressBookAppApplication.java
@@ -3,17 +3,17 @@ package com.example.AddressBookApp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableCaching
 @Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context=SpringApplication.run(AddressBookAppApplication.class, args);
-//		log.info("Address Book App Started");
-		log.info("Address Book App started in {} environment", context.getEnvironment().getProperty("environment"));
-		log.info("Address Book DB user is {}", context.getEnvironment().getProperty("spring.datasource.username"));
+		ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book App Started");
 	}
 
 }
