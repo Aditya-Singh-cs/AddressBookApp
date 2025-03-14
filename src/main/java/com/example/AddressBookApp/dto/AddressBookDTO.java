3a4ac08b1cf3d43062326e149fcce1bc10d4108b
Aditya-Singh-cs <aditya76596@gmail.com>
2025-03-14 14:43:15 +0530

    Added Validation

--- src/main/java/com/example/AddressBookApp/dto/AddressBookDTO.java
+++ src/main/java/com/example/AddressBookApp/dto/AddressBookDTO.java
@@ -1,4 +1,7 @@
 package com.example.AddressBookApp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Getter
@@ -6,11 +9,27 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AddressBookDTO {
+
     private Long id;
 
+    @NotBlank(message = "Name is required")
+    @Pattern(
+            regexp = "^[A-Z][a-zA-Z\\s]*$",
+            message = "Name must start with a capital letter and contain only letters and spaces"
+    )
     private String name;
 
+    @NotBlank(message = "Phone number is required")
+    @Pattern(
+            regexp = "^\\d{10}$",
+            message = "Phone number must be exactly 10 digits"
+    )
     private String phone;
 
+    @NotBlank(message = "Email is required")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
+            message = "Invalid email format"
+    )
     private String email;
 }
